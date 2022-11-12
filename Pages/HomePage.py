617826b788e77040f27ca2813b5d6ddf36149819
Rahulmodi3 <modirahul2019@gmail.com>
2022-11-12 16:59:41 +0530

    Update file after conflicted merge

--- Pages/HomePage.py
+++ Pages/HomePage.py
@@ -1,7 +1,9 @@
 def homePage2():
   print('This is gome page from dev 2')
 
-
 def VerifyHomePage(title):
   get_title= 'testing'
   assert title == get_title
+
+def verifyLogin(username,password):
+  print(username)
